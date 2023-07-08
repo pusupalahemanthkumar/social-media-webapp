0786d936841ea954687467273297682a47386301
Hemanth Kumar <pusupalahemanthkumar@gmail.com>
2023-07-08 19:02:20 +0530

    Initial frontend  project setup

---
+++ social-media-webapp/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Social Media App</h1>
+</template>
+
+<script>
+export default{
+}
+</script>
+
+
+<!-- Global Styling Here -->
+<style>
+
+/* Importing Google Fonts Here */
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: 'Roboto', sans-serif;
+  line-height: 1.5;
+}
+
+</style>
